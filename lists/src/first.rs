b87770d0a3da25b458758ffb1f8f851405c9867a
Rajat Rajput <rajputrajat@gmail.com>
2022-03-27 14:22:11 +0530

    bad list

---
+++ lists/src/first.rs
@@ -0,0 +1,41 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        Self { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            //next: self.head,
+            //next: Link::Empty,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
--- lists/src/lib.rs
+++ lists/src/lib.rs
@@ -1,8 +1,29 @@
+mod first;
+
 #[cfg(test)]
 mod tests {
+    use crate::first::List;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(2);
+        list.push(10);
+        list.push(233);
+
+        assert_eq!(list.pop(), Some(233));
+        assert_eq!(list.pop(), Some(10));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(2));
+
+        assert_eq!(list.pop(), None);
     }
 }
